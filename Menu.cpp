9b8559db3f47e1020ca5c1c5d53bdeded2808252
Ben <ryding.ben@gmail.com>
2012-07-23 19:21:15 -0400

    init

---
+++ Menu.cpp
@@ -0,0 +1,80 @@
+#include "Menu.h"
+
+
+/** Menu_Error **/
+
+std::fstream Menu_Error::log_file("menu_error_log.txt");
+int Menu_Error::ERROR_COUNT = 0;
+
+Menu_Error::Menu_Error(std::string e){
+    error = e;
+    ERROR_COUNT++;
+
+    time_t t = time(0);
+    tm* now = localtime(&t);
+    hour = now->tm_hour;
+    if(hour > 12) hour -= 12;
+    minute = now->tm_min;
+
+    day = now->tm_mday;
+    month = now->tm_mon + 1;
+    year = now->tm_year +1900;
+
+}
+
+
+void Menu_Error::log_error(){
+    if(ERROR_COUNT == 1){
+        log_file << "--------Menu Error Log--------\n\n";
+    }
+
+    log_file << ERROR_COUNT << ". ["
+             << month << "/" << day << "/" << year << "] "
+             << "[" << hour << ":" << minute << "] "
+             << error << std::endl;
+}
+
+void Menu_Error::print_error(){
+    std::cerr << "Exception thrown: "
+              << error << std::endl;
+}
+
+/***********************/
+/** Menu [base class] **/
+
+void Menu::print_menu() const{
+    std::ifstream dis(displayFile.c_str());
+    std::cout << file_to_string(dis);
+}
+
+void Menu::print_help() const{
+    std::ifstream help(helpFile.c_str());
+    std::cout << file_to_string(help);
+}
+
+std::string Menu::file_to_string(std::ifstream& in_file) const{
+    if(!in_file.good()) throw Menu_Error("Problem loading a text file.");
+    std::string display, temp;
+    while(std::getline(in_file, temp)){
+        display += temp + '\n';
+    }
+    return display;
+}
+
+Menu::Menu(std::string disFile_in, std::string helpFile_in){
+    displayFile = disFile_in;
+    helpFile = helpFile_in;
+
+}
+
+void Menu::get_command(){
+    std::cout << "\\>";
+    std::getline(std::cin, com);
+
+    while(com == "help"){
+        print_help();
+        std::cout << "\\>";
+        std::getline(std::cin, com);
+        std::cout << std::endl;
+    }
+}
---
+++ Menu.h
@@ -0,0 +1,48 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+
+class Menu_Error{
+public:
+    Menu_Error(std::string e);
+    void log_error();
+    void print_error();
+private:
+    Menu_Error() {}
+
+    std::string           error;
+    int                   hour, minute, day, month, year;
+    static int            ERROR_COUNT;
+    static std::fstream   log_file;
+};
+
+class Menu{
+public:
+    Menu(std::string disFile_in, std::string helpFile_in);
+
+    /** Interface **/
+    void print_menu() const;
+    void get_command();
+
+private:
+    //Constructor is private because helpFile and displayFile
+    //  MUST be initalized for this class to be used properly.
+    Menu(){}
+
+    std::string file_to_string(std::ifstream& in_file) const;
+    void print_help() const;
+
+    std::string     com,
+                    displayFile,
+                    helpFile;
+};
+
+
+
+#endif //MENU_H
